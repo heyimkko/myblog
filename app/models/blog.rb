a20474ee8fda147201899ad99218321ef59e18b9
Kevin Ko <enhasa@gmail.com>
2012-10-04 01:37:01 -0700

    Added Validations, Fixed Error Msgs

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,6 @@
 class Blog < ActiveRecord::Base
   attr_accessible :body, :time, :title
   
-  validates :title, :presence => true
+  validates :title, :presence => true, length: { maximum: 99 }
+  validates :body, :presence => true, length: { maximum: 999 }
 end
